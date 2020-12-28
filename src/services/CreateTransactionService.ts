06e46da6d456251ed5aa2440e41a807212d30473
Walleks Miranda <walleksmiranda@gmail.com>
2020-12-27 22:32:16 -0300

    :sparkles: Chanllenge finalized

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    // Validacao do tipo
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Type is invalid');
+    }
+
+    // Validacao da saida do saldo - tem o suficiente?
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new Error('You dont have enough!');
+    }
+    // Executa a funcao de inserir
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
   }
 }
 
